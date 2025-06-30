7656fe411158c886248cc265fa1f148d043bf8e2
Meital Lerner <meitalr555@gmail.com>
2025-06-30 10:22:08 +0300

    update Main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//שאלה 1
+        System.out.println("סיווג קשתות ב־DFS:");
+        DFSEdgeClassification.run();
+
+
+        // שאלה 2
+        int numVertices = 8;
+        SemiConnectedChecker checker = new SemiConnectedChecker(numVertices);
+
+        checker.addEdge(3, 7); // s → z
+        checker.addEdge(3, 4); // s → w
+        checker.addEdge(7, 6); // z → y
+        checker.addEdge(7, 4); // z → w
+        checker.addEdge(6, 5); // y → x
+        checker.addEdge(5, 7); // x → z
+        checker.addEdge(4, 5); // w → x
+        checker.addEdge(2, 3); // v → s
+        checker.addEdge(2, 4); // v → w
+        checker.addEdge(0, 2); // t → v
+        checker.addEdge(0, 1); // t → u
+        checker.addEdge(1, 0); // u → t
+        checker.addEdge(1, 2); // u → v
+
+        System.out.println("האם הגרף semi-connected?");
+        boolean result = checker.isSemiConnected();
+        System.out.println(result ? "כן " : "לא ");
+
     }
-
-
 }
